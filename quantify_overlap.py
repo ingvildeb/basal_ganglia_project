0fe09b9f54ce7ea803b4c1cbe90fe91aa2bff549
Ingvild Bjerke <i.e.bjerke@medisin.uio.no>
2023-04-27 19:20:13 +0200

    Update quantify_overlap.py
    Ran Swanson atlas. Added some new code to make a summary table of all the overlap. Realized need to re-write code and get the load for the summaries calculated after sectionwise object pixels and region pixels are added together, so it adds up to 1. Need to clean up code as well and remove paxinos references from variable names.

--- quantify_overlap.py
+++ quantify_overlap.py
@@ -107,61 +107,138 @@ new_colors = set_region_colors("Z:/NESYS_Lab/PostDoc_project_Bjerke/Manuscripts/
 new_color_dict = dict(zip(WHS_colors.Region, new_colors))
 
 
-
+WHS_regions = []
 
 for paxregion in pax_region_list:
 
-    if paxregion == "Lateral Accumbens shell":
         
-        summary = "Z:/NESYS_Lab/PostDoc_project_Bjerke/Manuscripts/WHSv4_Basal ganglia/04_Material/quantitative_overlap_analysis/" + paxregion + "_summary.xlsx"
-        summary = pd.read_excel(summary)
-        unique_sections = unique_list(summary["Section"])
-        rostral_sections, mid_sections, caudal_sections = split_array(unique_sections)
-        
-        segment_list = []
-        for s in summary["Section"]:
-            if s in rostral_sections:
-                segment = "Rostral"
-            if s in mid_sections:
-                segment = "Middle"
-            if s in caudal_sections:
-                segment = "Caudal"
-            segment_list.append(segment)
-        
-        summary["Segment"] = segment_list
-        
-        summary_mean = summary.groupby(["Segment", "Region"]).mean()
+    summary = "Z:/NESYS_Lab/PostDoc_project_Bjerke/Manuscripts/WHSv4_Basal ganglia/04_Material/quantitative_overlap_analysis/Paxinos_v6/" + paxregion + "_summary.xlsx"
+    summary = pd.read_excel(summary)
+    unique_sections = unique_list(summary["Section"])
+    rostral_sections, mid_sections, caudal_sections = split_array(unique_sections)
     
-        summary_mean = summary_mean.reset_index()
-        
-        props = summary_mean["Pax-in-WHS_Proportion"]
-        regs = summary_mean["Region"]
-        to_plot = summary_mean[["Segment", "Region", "Pax-in-WHS_Proportion"]]
+    segment_list = []
+    for s in summary["Section"]:
+        if s in rostral_sections:
+            segment = "Rostral"
+        if s in mid_sections:
+            segment = "Middle"
+        if s in caudal_sections:
+            segment = "Caudal"
+        segment_list.append(segment)
     
-        
+    summary["Segment"] = segment_list
     
-        segments = ["Rostral", "Middle", "Caudal"]
-        
-        fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(12, 5))
+    #summary_mean = summary.groupby(["Segment", "Region"]).mean()
+    summary_mean = summary.groupby(["Region"]).mean()
+
+    summary_mean = summary_mean.reset_index()
     
-        
-        for segment_i, ax in zip(segments, axs.flat):
-            to_plot_b = to_plot[to_plot['Segment'] == segment_i]
-            labels = to_plot_b['Region']
-            texts, autotexts, wedges = ax.pie(to_plot_b['Pax-in-WHS_Proportion'], shadow=False, colors=[new_color_dict[key] for key in labels], startangle=90, textprops=dict(color="black"), autopct='')
+    props = summary_mean["Pax-in-WHS_Proportion"]
+    regs = summary_mean["Region"]
+    #to_plot = summary_mean[["Segment", "Region", "Pax-in-WHS_Proportion"]]
+    to_plot = summary_mean[["Region", "Pax-in-WHS_Proportion"]]
+    to_plot["PaxRegion"] = paxregion
+
+    to_plot.to_excel(r"Z:\NESYS_Lab\PostDoc_project_Bjerke\Manuscripts\WHSv4_Basal ganglia\04_Material\quantitative_overlap_analysis\Paxinos_v6\\" + paxregion + "_summary_segments_combined.xlsx")
     
-            ax.set_title(segment_i, size=16, weight="bold", y=-0.05)
+    WHS_regions.append(regs.tolist())
+
+    segments = ["Rostral", "Middle", "Caudal"]
     
+    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(12, 5))
+
+    
+    for segment_i, ax in zip(segments, axs.flat):
+        to_plot_b = to_plot[to_plot['Segment'] == segment_i]
+        labels = to_plot_b['Region']
+        texts, autotexts, wedges = ax.pie(to_plot_b['Pax-in-WHS_Proportion'], shadow=False, colors=[new_color_dict[key] for key in labels], startangle=90, textprops=dict(color="black"), autopct='')
+
+        ax.set_title(segment_i, size=16, weight="bold", y=-0.05)
+
+    
+        ax.legend(texts, labels, loc=6, mode="expand", bbox_to_anchor=(0, -0.30, 1, 0))
         
-            ax.legend(texts, labels, loc=6, mode="expand", bbox_to_anchor=(0, -0.30, 1, 0))
-            
-        plt.tight_layout()
-        fig.suptitle(paxregion)
-        plt.savefig("Z:/NESYS_Lab/PostDoc_project_Bjerke/Manuscripts/WHSv4_Basal ganglia/04_Material/quantitative_overlap_analysis/" + paxregion + '_pie.svg', bbox_inches='tight', pad_inches=0.05, dpi=600)
-        plt.show()
+    plt.tight_layout()
+    fig.suptitle(paxregion)
+    plt.savefig("Z:/NESYS_Lab/PostDoc_project_Bjerke/Manuscripts/WHSv4_Basal ganglia/04_Material/quantitative_overlap_analysis/Paxinos_v6/" + paxregion + '_pie.svg', bbox_inches='tight', pad_inches=0.05, dpi=600)
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+WHS_regions = []
+
+for paxregion in pax_region_list:
         
-    else:
-        continue
+    report = r"Z:\NESYS_Lab\PostDoc_project_Bjerke\Manuscripts\WHSv4_Basal ganglia\04_Material\quantitative_overlap_analysis\Paxinos_v6\\" + paxregion + "_summary_segments_combined.xlsx"
+    report = pd.read_excel(report)
+    
+    regions = report["Region"].tolist()
+    
+    WHS_regions.append(regions)
+
+
+
+
+WHS_regions_flat = [i for sublist in WHS_regions for i in sublist]
+unique_WHS_regions = unique_list(WHS_regions_flat)
+
+table = pd.DataFrame(unique_WHS_regions)
+table.columns = ["Region"]
+
+
+for paxregion in pax_region_list:
+    
+    report = r"Z:\NESYS_Lab\PostDoc_project_Bjerke\Manuscripts\WHSv4_Basal ganglia\04_Material\quantitative_overlap_analysis\Paxinos_v6\\" + paxregion + "_summary_segments_combined.xlsx"
+    report = pd.read_excel(report)
+    
+    regs_in_report = report["Region"].tolist()
+    
+    table[paxregion] = ""
+    
+    for reg in regs_in_report:
+        selection = report.loc[report["Region"] == reg]
+        value = selection["Pax-in-WHS_Proportion"]
+        value = float(value)
+        paxreg = selection["PaxRegion"]
+
+                
+        
+        regindex = table.index[table["Region"] == reg]
+        table.loc[regindex, paxreg] = value
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
                 
 
